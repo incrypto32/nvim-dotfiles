bf77312b37b75f62a025d5739c638ee64bf53bb1
Krishnanand V P <vpkrishnanand@gmail.com>
2025-06-01 12:03:52 +0530

    feat: add tint.nvim plugin for window tinting and configure fidget.nvim event loading

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -20,10 +20,42 @@ return {
   {
     "karb94/neoscroll.nvim",
     opts = {},
-  }
-  ,
+  },
   {
     "j-hui/fidget.nvim",
-    opts = {},
+    event = "VeryLazy",
+    opts = {
+      -- log_level = 'debug',
+    },
+  },
+  {
+    "levouh/tint.nvim",
+    config = function()
+      require("tint").setup({
+        tint = -70,
+        window_ignore_function = function(winid)
+          local bufid = vim.api.nvim_win_get_buf(winid)
+          local buftype = vim.api.nvim_buf_get_option(bufid, "buftype")
+          local floating = vim.api.nvim_win_get_config(winid).relative ~= ""
+
+          -- Do not tint `terminal` or floating windows, tint everything else
+          return buftype == "terminal" or floating
+        end
+      })
+    end
   }
+  -- {
+  --   "sunjon/shade.nvim",
+  --   config = function()
+  --     require 'shade'.setup({
+  --       overlay_opacity = 50,
+  --       opacity_step = 1,
+  --       keys = {
+  --         brightness_up   = '<C-Up>',
+  --         brightness_down = '<C-Down>',
+  --         toggle          = '<Leader>s',
+  --       }
+  --     })
+  --   end
+  -- }
 }
