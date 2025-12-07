8d393b884a81aca52a06c9f0d5e2c09b354a3471
Krishnanand V P <vpkrishnanand@gmail.com>
2025-12-07 11:36:01 +0000

    enable osc52 clipboard

--- lua/options.lua
+++ lua/options.lua
@@ -2,3 +2,15 @@ require "nvchad.options"
 
 local o = vim.o
 o.autoread = true
+
+vim.g.clipboard = {
+    name = 'OSC 52',
+    copy = {
+      ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+      ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+    },
+    paste = {
+      ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+      ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+    },
+}
